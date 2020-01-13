e0785e5dcb3f2b1fcba81d149deb9a8470910cec
Iorgen Wildrik <w.drik@outlook.com>
2020-01-13 20:54:32 -0300

    Finalizando primeira etapa do backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -17,6 +17,6 @@ routes.post("/spots", upload.single("thumbnail"), SpotController.store);
 
 routes.get("/dashboard", DashboardController.show);
 
-routes.get("/spots/:spot_id/bookings", BookingController.store);
+routes.post("/spots/:spot_id/bookings", BookingController.store);
 
 module.exports = routes;
