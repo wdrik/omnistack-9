c2c92498763765e4e0d55a653f1a16e4ffdaf47b
Iorgen Wildrik <w.drik@outlook.com>
2020-01-13 08:19:43 -0300

    Implementando backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello world!" });
-});
+mongoose.connect(
+  "mongodb+srv://omnistack:1234@cluster0-nhv9j.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
