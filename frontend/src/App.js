ab22fc9d21adad79f32f8ef2a9b7a81702e4f648
Iorgen Wildrik <w.drik@outlook.com>
2020-01-14 22:27:56 -0300

    Layout da tela de Login!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
 import "./App.css";
 
+import camera from "./assets/camera.svg";
+import logo from "./assets/logo.svg";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="container">
+      <img src={logo} alt="Camera" />
+      <img src={camera} alt="Camera" />
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre
+          <strong> talentos </strong>
+          para sua empresa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input type="email" id="email" placeholder="Seu melhor e-mail" />
+
+          <button type="submit" className="btn">
+            Entrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
