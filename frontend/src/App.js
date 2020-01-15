c50b8e07e2010b36c5e48039ddcfdc52b098287b
Iorgen Wildrik <w.drik@outlook.com>
2020-01-15 07:30:59 -0300

    Prosseguindo com frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,19 @@
-import React, { useState } from "react";
-import api from "./services/api";
+import React from "react";
 import "./App.css";
 
 import camera from "./assets/camera.svg";
 import logo from "./assets/logo.svg";
 
+import Routes from "./routes";
+
 function App() {
-  const [email, setEmail] = useState("");
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    const { data } = await api.post("/sessions", { email });
-
-    console.log(data);
-  }
-
   return (
     <div className="container">
-      <img src={logo} alt="Camera" />
       <img src={camera} alt="Camera" />
+      <img src={logo} alt="Camera" />
 
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre
-          <strong> talentos </strong>
-          para sua empresa
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">
-            E-MAIL <span>*</span>
-          </label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Seu melhor e-mail"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-
-          <button type="submit" className="btn">
-            Entrar
-          </button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
